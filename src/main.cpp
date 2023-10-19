30733b0b09715b5b99df31f0beebf9a601cfa032
Eric Canton <eric.d.canton@gmail.com>
2023-10-19 16:48:31 -0500

    start adding executable subproject.

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <sot.h>
+using namespace sot;
+
+int main(int argc, char **argv)
+{
+    int dim = 10;
+    int maxEvals = 501; // Evaluation budget
+    setSeedRandom();    // Set the SOT seed ramdomly
+
+    std::shared_ptr<Problem> data(std::make_shared<Ackley>(dim));
+    std::shared_ptr<ExpDesign> slhd(std::make_shared<SLHD>(2 * (dim + 1), dim));
+    std::shared_ptr<Surrogate> rbf(std::make_shared<CubicRBF>(maxEvals, dim, data->lBounds(), data->uBounds()));
+    std::shared_ptr<Sampling> dycors(std::make_shared<DYCORS<>>(data, rbf, 100 * dim, maxEvals - slhd->numPoints()));
+
+    Optimizer opt(data, slhd, rbf, dycors, maxEvals);
+    Result res = opt.run();
+
+    std::cout << "Best value found: " << res.fBest() << std::endl;
+    std::cout << "Best solution found: " << res.xBest().t() << std::endl;
+}
